d8faedbbfa40e4fc15699df637a5ca67e05a4427
howardchi <howardchi@fougood.com>
2015-10-04 22:37:27 +0800

    add line

--- webpack.config.js
+++ webpack.config.js
@@ -14,51 +14,13 @@ var js_dist_min = path.join(__dirname, './dist/min');
 var ENV = !!(+process.env.NODE_ENV || 0);
 
 module.exports = [{
-  name: 'coreComponent',
-  devtool: ENV ? "source-map": '',
-  entry: {
-    xaxis_garbage: './xaxis/es6/xaxis_garbage.js'
-  },
-
-  output: {
-    path: ENV ? js_dist_min + '/es6'  : js_dist + '/es6',
-    filename: ENV ? '[name].min.js': '[name].js'
-  },
-
-  module: {
-    loaders: [
-      {
-        test: [/\.js$/],
-        loaders: ["react-hot", "babel-loader?stage=0"],
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      }
-    ],
-  },
-
-  resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx']
-  },
-
-  plugins: ENV ? [
-    new webpack.optimize.UglifyJsPlugin({minimize: true}),
-    new webpack.ProvidePlugin({
-      'd3': 'd3'
-    })
-  ]: [
-    new webpack.ProvidePlugin({
-      'd3': 'd3'
-    })
-  ]
-},{
   name: 'coreComponent-es5',
   devtool: ENV ? "source-map": '',
   entry: {
     xaxis_garbage: './xaxis/es5/xaxis_garbage.js',
     xaxis_click: './xaxis/es5/xaxis_click.js',
-    legend_click: './legend/legend_click.jsx'
+    legend_click: './legend/legend_click.jsx',
+    line_garbage: './line/es5/line_garbage.js'
   },
 
   output: {
